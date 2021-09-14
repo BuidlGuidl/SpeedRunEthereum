b1b1a593e49d0c94da9d3cb417b89474738eef73
Daniel Gonzalez Reina <dangonrei@gmail.com>
2021-09-14 14:07:15 +0200

    Use normal react-router Link

--- packages/react-app/src/components/ChallengeCard.jsx
+++ packages/react-app/src/components/ChallengeCard.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Typography } from "antd";
 
-const { Text, Link: AntdLink } = Typography;
+const { Text } = Typography;
 
 const ChallengeCard = ({ challengeId, challengeInfo, submissionInfo }) => (
   <div style={{ display: "inline-block", opacity: challengeInfo.disabled ? 0.5 : 1 }}>
     {challengeInfo.disabled ? (
       <Text>{challengeInfo.label}</Text>
     ) : (
-      <Link to={`/challenge/${challengeId}`} component={AntdLink}>
-        {challengeInfo.label}
-      </Link>
+      <Link to={`/challenge/${challengeId}`}>{challengeInfo.label}</Link>
     )}
     {submissionInfo && <Text> [{submissionInfo.status}]</Text>}
     {challengeInfo.disabled && <Text> (disabled)</Text>}
