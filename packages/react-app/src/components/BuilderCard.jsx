c0eb5d6f84191ee5b757db2adcc94c3a53d1f127
Asgeir <asgeir@cluda.com>
2021-09-16 13:05:47 +0200

    Update packages/react-app/src/components/BuilderCard.jsx
    Co-authored-by: Daniel González Reina <dangonrei@gmail.com>

--- packages/react-app/src/components/BuilderCard.jsx
+++ packages/react-app/src/components/BuilderCard.jsx
@@ -3,10 +3,8 @@ import { Link } from "react-router-dom";
 import Address from "./Address";
 
 const BuilderCard = ({ builder, mainnetProvider }) => (
-  <Link to={`/builders/${builder.id}`}>
-    <div style={{ display: "inline-block" }}>
-      <Address address={builder.id} ensProvider={mainnetProvider} />
-    </div>
+  <Link to={`/builders/${builder.id}`} style={{ display: "inline-block" }}>
+    <Address address={builder.id} ensProvider={mainnetProvider} />
   </Link>
 );
 
