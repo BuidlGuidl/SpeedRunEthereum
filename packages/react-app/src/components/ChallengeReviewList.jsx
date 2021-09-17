54fed3dbc7455b46ca33fb635705893d681fa3fb
sogasg <sogasg@gmail.com>
2021-09-17 14:14:13 +0200

    Add the ability for reviewers to add a comment

--- packages/react-app/src/components/ChallengeReviewList.jsx
+++ packages/react-app/src/components/ChallengeReviewList.jsx
@@ -1,36 +1,61 @@
 import React from "react";
-import { List, Button } from "antd";
+import { List, Button, Input } from "antd";
 import { challengeInfo } from "../data/challenges";
 
 export default function ChallengeReviewList({ challengeSubmissions, isLoading, approveClick, rejectClick }) {
+  const [commentMap, setCommentMap] = React.useState({});
   return (
     <List
       loading={isLoading}
-      itemLayout="horizontal"
+      itemLayout="vertical"
       dataSource={challengeSubmissions}
       renderItem={challenge => (
         <List.Item
           actions={[
-            <Button type="primary" onClick={() => approveClick(challenge.userAddress, challenge.id, "")}>
-              Approve Button
+            <Button type="link" href={challenge.branchUrl} target="_blank">
+              Code
             </Button>,
-            <Button danger onClick={() => rejectClick(challenge.userAddress, challenge.id, "")}>
-              Reject
+            <Button type="link" href={challenge.deployedUrl} target="_blank">
+              Live Demo
             </Button>,
           ]}
+          extra={
+            <div>
+              <Input.TextArea
+                onChange={e => {
+                  const currentCommentMap = commentMap;
+                  currentCommentMap[challenge.userAddress + challenge.id] = e.target.value;
+                  setCommentMap(currentCommentMap);
+                }}
+                value={commentMap[challenge.userAddress + challenge.id]}
+                placeholder="Comment for builder"
+                style={{ marginBottom: 10 }}
+                rows={2}
+              />
+              <Button
+                type="primary"
+                style={{ marginRight: 10 }}
+                onClick={() =>
+                  approveClick(challenge.userAddress, challenge.id, commentMap[challenge.userAddress + challenge.id])
+                }
+              >
+                Approve Button
+              </Button>
+              <Button
+                danger
+                onClick={() =>
+                  rejectClick(challenge.userAddress, challenge.id, commentMap[challenge.userAddress + challenge.id])
+                }
+              >
+                Reject
+              </Button>
+            </div>
+          }
         >
           <List.Item.Meta
             title={challengeInfo[challenge.id].label}
             description={challengeInfo[challenge.id].description}
           />
-          <div>
-            <Button type="link" href={challenge.branchUrl} target="_blank">
-              Code
-            </Button>
-            <Button type="link" href={challenge.deployedUrl} target="_blank">
-              Live Demo
-            </Button>
-          </div>
         </List.Item>
       )}
     />
