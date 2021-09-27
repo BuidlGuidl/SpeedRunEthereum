9dc630778d3f022ede5f2ce8762d24b1728f24b7
Daniel Gonzalez Reina <dangonrei@gmail.com>
2021-09-27 18:02:54 +0200

    Update jsdoc for the middleware

--- packages/backend/middlewares/auth.js
+++ packages/backend/middlewares/auth.js
@@ -1,16 +1,5 @@
 /**
- * Middleware to validate any request that needs to be authorized.
- *
- * For the verification to work properly, two headers have to be sent with the request:
- * ```js
- * headers: {
- *   authorization: `token ${token}`, // token being the one generated at `POST /sign`
- *   address, // address being the blockchain address of the user making the request
- * }
- * ```
- *
- * See /packages/react-app/views/JwtTest.jsx for a code example.
- * Testing of the middleware can be done at /jwt-test on the client side
+ * Middleware that adds to the request the address sent in the headers.
  *
  * @param {Express.Request} req
  * @param {Express.Response} res
