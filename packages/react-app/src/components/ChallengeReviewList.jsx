948b705ef8f83449a93422b839116751298c2fb1
carletex <oceanrdn@gmail.com>
2021-10-13 11:01:30 +0200

    Chakra UI: review Challenges

--- packages/react-app/src/components/ChallengeReviewList.jsx
+++ packages/react-app/src/components/ChallengeReviewList.jsx
@@ -1,26 +1,28 @@
 import React from "react";
+import { chakra, Button, HStack, Link, VStack, Textarea } from "@chakra-ui/react";
 import { challengeInfo } from "../data/challenges";
 
 export default function ChallengeReviewList({ challengeSubmissions, isLoading, approveClick, rejectClick }) {
   const [commentMap, setCommentMap] = React.useState({});
 
   return (
-    <ul>
+    <VStack as="ul" spacing={6}>
       {challengeSubmissions.map(challenge => (
-        <li key={challenge.userAddress + challenge.id}>
+        <chakra.li key={challenge.userAddress + challenge.id} w="100%">
           <div>
             <strong>{challengeInfo[challenge.id].label}</strong>
             <p>{challengeInfo[challenge.id].description}</p>
           </div>
-          <a href={challenge.branchUrl} target="_blank" rel="noopener noreferrer">
-            Code
-          </a>
-          ,
-          <a href={challenge.deployedUrl} target="_blank" rel="noopener noreferrer">
-            Live Demo
-          </a>
+          <HStack spacing={3}>
+            <Link href={challenge.branchUrl} color="teal.500" target="_blank" rel="noopener noreferrer">
+              Code
+            </Link>
+            <Link href={challenge.deployedUrl} color="teal.500" target="_blank" rel="noopener noreferrer">
+              Live Demo
+            </Link>
+          </HStack>
           <div>
-            <textarea
+            <Textarea
               onChange={e => {
                 const value = e.target.value;
                 setCommentMap(preCommentMap => {
@@ -33,29 +35,33 @@ export default function ChallengeReviewList({ challengeSubmissions, isLoading, a
               style={{ marginBottom: 10 }}
               rows={2}
             />
-            <button
-              type="button"
-              disabled={isLoading}
-              style={{ marginRight: 10 }}
-              onClick={() =>
-                approveClick(challenge.userAddress, challenge.id, commentMap[challenge.userAddress + challenge.id])
-              }
-            >
-              Approve Button
-            </button>
-            <button
-              type="button"
-              disabled={isLoading}
-              className="danger"
-              onClick={() =>
-                rejectClick(challenge.userAddress, challenge.id, commentMap[challenge.userAddress + challenge.id])
-              }
-            >
-              Reject
-            </button>
+            <HStack spacing="12px">
+              <Button
+                type="button"
+                colorScheme="red"
+                disabled={isLoading}
+                className="danger"
+                onClick={() =>
+                  rejectClick(challenge.userAddress, challenge.id, commentMap[challenge.userAddress + challenge.id])
+                }
+              >
+                Reject
+              </Button>
+              <Button
+                type="button"
+                colorScheme="blue"
+                disabled={isLoading}
+                style={{ marginRight: 10 }}
+                onClick={() =>
+                  approveClick(challenge.userAddress, challenge.id, commentMap[challenge.userAddress + challenge.id])
+                }
+              >
+                Approve Button
+              </Button>
+            </HStack>
           </div>
-        </li>
+        </chakra.li>
       ))}
-    </ul>
+    </VStack>
   );
 }
