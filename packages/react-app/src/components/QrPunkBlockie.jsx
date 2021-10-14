c7449210492d086b85a2149b4ac190ec4aad0ce3
carletex <oceanrdn@gmail.com>
2021-10-14 15:55:46 +0200

    Use new QrPunk implementation + clean up

--- packages/react-app/src/components/QrPunkBlockie.jsx
+++ packages/react-app/src/components/QrPunkBlockie.jsx
@@ -1,104 +1,78 @@
 import React from "react";
-import QR from "qrcode.react";
 import Blockies from "react-blockies";
 
+// ToDo. Implement QR.
 export default function QRPunkBlockie(props) {
+  // Punk size with scale = 1
   const punkSize = 112;
+  // Punk size pixels in the original file
+  const originalPunkSize = 24;
 
-  const part1 = props.address && props.address.substr(2, 20);
-  const part2 = props.address && props.address.substr(22);
+  const part1 = props.address?.slice(2, 22);
+  const part2 = props.address?.slice(-20);
+
   const x = parseInt(part1, 16) % 100;
   const y = parseInt(part2, 16) % 100;
 
+  const scale = props.scale ?? 1;
+  const punkRatio = punkSize / originalPunkSize;
+
   return (
-    <div
+    <button
+      type="button"
       style={{
-        width: punkSize * props.scale ?? 1,
-        height: punkSize * props.scale ?? 1,
+        width: punkSize * scale,
+        height: punkSize * scale,
+        margin: 0,
+        display: "block",
+      }}
+      onClick={() => {
+        // ToDo. Prop to control this (we might not want to copy when clicking the avatar)
+        // ToDo. Show alert / notification message.
+        navigator.clipboard
+          .writeText(props.address)
+          .then(() => {
+            console.log("successfully copied address to clipboard", props.address);
+          })
+          .catch(err => {
+            console.log("error copying address");
+            console.log(err);
+          });
       }}
     >
       <div
         style={{
-          transform: "scale(" + (props.scale ? props.scale : "1") + ")",
-          transformOrigin: "0",
-        }}
-        onClick={() => {
-          const el = document.createElement("textarea");
-          el.value = props.address;
-          document.body.appendChild(el);
-          el.select();
-          document.execCommand("copy");
-          document.body.removeChild(el);
-          const iconPunkSize = 40;
-          // ToDo. Prop to control this (we might not want to copy when clicking the avatar)
-          // ToDo. Show alert / notification message.
-          console.log(
-            <span style={{ position: "relative" }}>
-              Copied Address
-              <div style={{ position: "absolute", left: -60, top: -14 }}>
-                <div
-                  style={{ position: "relative", width: iconPunkSize, height: iconPunkSize - 1, overflow: "hidden" }}
-                >
-                  <img
-                    alt="avatar punk"
-                    src="/punks.png"
-                    style={{
-                      position: "absolute",
-                      left: -iconPunkSize * x,
-                      top: -iconPunkSize * y,
-                      width: iconPunkSize * 100,
-                      height: iconPunkSize * 100,
-                      imageRendering: "pixelated",
-                    }}
-                  />
-                </div>
-              </div>
-            </span>,
-          );
+          position: "relative",
         }}
       >
         <div
+          id="blockie"
           style={{
-            position: "absolute",
             opacity: 0.4,
-            left: 0,
             top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
           }}
         >
-          <Blockies seed={props.address.toLowerCase()} scale={punkSize / 8} />
+          <Blockies seed={props.address.toLowerCase()} scale={(punkSize / 8) * scale} />
         </div>
-
-        <div style={{ position: "absolute", left: 0, top: 0 }}>
-          <div style={{ position: "relative", width: punkSize, height: punkSize - 1, overflow: "hidden" }}>
-            <img
-              alt="avatar punk"
-              src="/punks.png"
-              style={{
-                position: "absolute",
-                left: -punkSize * x,
-                top: -punkSize * y - 1,
-                width: punkSize * 100,
-                height: punkSize * 100,
-                imageRendering: "pixelated",
-                maxWidth: "none",
-              }}
-            />
-          </div>
-        </div>
-
-        {props.withQr ? (
-          <QR
-            level="H"
-            includeMargin={false}
-            // ethereum:0x34aA3F359A9D614239015126635CE7732c18fDF3
-            value={props.address ? "ethereum:" + props.address : ""}
-            size={320}
-            imageSettings={{ width: 105, height: 105, excavate: true }}
-          />
-        ) : (
-          ""
-        )}
+        <div
+          id="punk"
+          style={{
+            position: "absolute",
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 0,
+            backgroundImage: "url(/punks.png)",
+            backgroundSize: `${2400 * punkRatio * scale}px ${2400 * punkRatio * scale}px`,
+            backgroundPosition: `${-punkSize * x * scale}px ${-punkSize * y * scale}px`,
+            backgroundRepeat: "no-repeat",
+            imageRendering: "pixelated",
+          }}
+        />
       </div>
-    </div>
+    </button>
   );
 }
