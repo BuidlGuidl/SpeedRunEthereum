910631401902e5a7c645efe7758fa0d599961233
carletex <oceanrdn@gmail.com>
2021-10-19 11:45:43 +0200

    Basic UI blueprint for All builders page

--- packages/react-app/src/components/BuilderCard.jsx
+++ packages/react-app/src/components/BuilderCard.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { Link as RouteLink } from "react-router-dom";
-import { Link } from "@chakra-ui/react";
+import { Link, Td } from "@chakra-ui/react";
 import Address from "./Address";
 
-const BuilderCard = ({ builder, mainnetProvider }) => (
-  <Link as={RouteLink} to={`/builders/${builder.id}`} pos="relative">
-    <Address address={builder.id} ensProvider={mainnetProvider} scale="0.4" />
-  </Link>
-);
+const BuilderCard = ({ builder, mainnetProvider }) => {
+  const acceptedChallenges =
+    builder.challenges &&
+    // ToDo. Move "ACCEPTED" to a shared constant.
+    Object.keys(builder.challenges).filter(challengeId => builder.challenges[challengeId].status === "ACCEPTED");
+
+  return (
+    <>
+      <Td>
+        <Link as={RouteLink} to={`/builders/${builder.id}`} pos="relative">
+          <Address address={builder.id} ensProvider={mainnetProvider} scale="0.4" fontSize="16" />
+        </Link>
+      </Td>
+      <Td>-</Td>
+      <Td>{acceptedChallenges ? acceptedChallenges.length : 0}</Td>
+    </>
+  );
+};
 
 export default BuilderCard;
--- packages/react-app/src/views/BuilderListView.jsx
+++ packages/react-app/src/views/BuilderListView.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { chakra, Container, Heading } from "@chakra-ui/react";
+import { Container, Heading, Link, Text, Table, Thead, Tbody, Tr, Th } from "@chakra-ui/react";
 import BuilderCard from "../components/BuilderCard";
 
 const serverPath = "/builders";
@@ -18,15 +18,36 @@ export default function BuilderListView({ serverUrl, mainnetProvider }) {
   }, [serverUrl]);
 
   return (
-    <Container>
-      <Heading as="h1">scaffold-eth Builders</Heading>
-      <ul style={{ listStyle: "none" }}>
-        {builders.map(builder => (
-          <chakra.li pb={4} key={builder.id}>
-            <BuilderCard builder={builder} mainnetProvider={mainnetProvider} />
-          </chakra.li>
-        ))}
-      </ul>
+    <Container maxW="container.lg">
+      <Container maxW="container.md" centerContent>
+        <Heading as="h1" mb="2">
+          All Builders
+        </Heading>
+        <Text color="gray.700" textAlign="center">
+          List of Ethereum builders creating products, prototypes, and tutorials with{" "}
+          <Link href="https://github.com/scaffold-eth/scaffold-eth" color="teal.500" isExternal>
+            scaffold-eth
+          </Link>
+          .
+        </Text>
+        <Text color="gray.700" mb="6">You can fund Eth development sending Eth to any stream.</Text>
+      </Container>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Builder</Th>
+            <Th>Builds</Th>
+            <Th>Challenges</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {builders.map(builder => (
+            <Tr key={builder.id}>
+              <BuilderCard builder={builder} mainnetProvider={mainnetProvider} />
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </Container>
   );
 }
