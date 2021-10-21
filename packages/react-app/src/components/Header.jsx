d4410505da04b666ed3ad0e2d659562bb0ee5802
carletex <oceanrdn@gmail.com>
2021-10-21 09:09:36 +0200

    Make the header take all the available space

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { chakra, Box, Container, Flex, HStack, Spacer } from "@chakra-ui/react";
+import { chakra, Box, Flex, HStack, Spacer } from "@chakra-ui/react";
 import { Account } from "./index";
 import { USER_ROLES } from "../helpers/constants";
 
@@ -18,60 +18,58 @@ export default function Header({
     injectedProvider && injectedProvider.getSigner && injectedProvider.getSigner()._isSigner;
 
   return (
-    <Box borderBottom="1px" borderColor="gray.200" mb={10}>
-      <Container maxW="container.xl" h={20}>
-        <Flex align="center" h="full" fontWeight="semibold">
-          <HStack as="ul" spacing={9} style={{ listStyle: "none" }}>
-            <chakra.li key="/">
-              <NavLink to="/" exact>
-                <span role="img" aria-label="castle icon">
-                  🏰
-                </span>{" "}
-                <strong>BuidlGuidl</strong>
-              </NavLink>
-            </chakra.li>
-            <chakra.li key="/builders" color="gray.600" _hover={{ color: "gray.700" }}>
+    <Box borderBottom="1px" borderColor="gray.200" mb={10} px={8} h={20}>
+      <Flex align="center" h="full" fontWeight="semibold">
+        <HStack as="ul" spacing={9} style={{ listStyle: "none" }}>
+          <chakra.li key="/">
+            <NavLink to="/" exact>
+              <span role="img" aria-label="castle icon">
+                🏰
+              </span>{" "}
+              <strong>BuidlGuidl</strong>
+            </NavLink>
+          </chakra.li>
+          <chakra.li key="/builders" color="gray.600" _hover={{ color: "gray.700" }}>
+            <NavLink
+              to="/builders"
+              exact
+              activeStyle={{
+                color: "var(--chakra-colors-gray-700)",
+              }}
+            >
+              Builders
+            </NavLink>
+          </chakra.li>
+          {USER_ROLES.admin === userRole && (
+            <chakra.li key="/challenge-review" color="gray.600" _hover={{ color: "gray.700" }}>
               <NavLink
-                to="/builders"
+                to="/challenge-review"
                 exact
                 activeStyle={{
                   color: "var(--chakra-colors-gray-700)",
                 }}
               >
-                Builders
+                Review Challenges
               </NavLink>
             </chakra.li>
-            {USER_ROLES.admin === userRole && (
-              <chakra.li key="/challenge-review" color="gray.600" _hover={{ color: "gray.700" }}>
-                <NavLink
-                  to="/challenge-review"
-                  exact
-                  activeStyle={{
-                    color: "var(--chakra-colors-gray-700)",
-                  }}
-                >
-                  Review Challenges
-                </NavLink>
-              </chakra.li>
-            )}
-          </HStack>
-          <Spacer />
-          <Account
-            address={address}
-            connectText="Connect Wallet"
-            ensProvider={mainnetProvider}
-            isWalletConnected={isSignerProviderConnected}
-            loadWeb3Modal={loadWeb3Modal}
-            logoutOfWeb3Modal={() => {
-              logoutOfWeb3Modal();
-              setUserRole(USER_ROLES.anonymous);
-            }}
-            setUserRole={setUserRole}
-            userProvider={userProvider}
-            userRole={userRole}
-          />
-        </Flex>
-      </Container>
+          )}
+        </HStack>
+        <Spacer />
+        <Account
+          address={address}
+          connectText="Connect Wallet"
+          ensProvider={mainnetProvider}
+          isWalletConnected={isSignerProviderConnected}
+          loadWeb3Modal={loadWeb3Modal}
+          logoutOfWeb3Modal={() => {
+            logoutOfWeb3Modal();
+            setUserRole(USER_ROLES.anonymous);
+          }}
+          setUserRole={setUserRole}
+          userProvider={userProvider}
+          userRole={userRole}
+        />
+      </Flex>
     </Box>
   );
 }
