f0a2a45b70b346a552981b58653f6fcf9395b1c1
Carlos Sánchez <oceanrdn@gmail.com>
2021-11-10 11:44:54 +0100

    React markdown. Test with simple-nft-example

--- packages/react-app/src/views/ChallengeDetailView.jsx
+++ packages/react-app/src/views/ChallengeDetailView.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
-import { Button, Container, Heading, Text } from "@chakra-ui/react";
+import { Button, Container, Heading } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
+import ReactMarkdown from "react-markdown";
 import { challengeInfo } from "../data/challenges";
 import ChallengeSubmission from "../components/ChallengeSubmission";
 
 export default function ChallengeDetailView({ serverUrl, address, userProvider }) {
+  const [description, setDescription] = useState("");
   const { challengeId } = useParams();
   const history = useHistory();
-
   const challenge = challengeInfo[challengeId];
+
+  // Fetch challenge description
+  useEffect(() => {
+    import(`../data/challenges/${challengeId}.md`)
+      .then(file => {
+        fetch(file.default)
+          .then(content => content.text())
+          .then(text => setDescription(text))
+          .catch(() => setDescription(challenge.description));
+      })
+      .catch(() => setDescription(challenge.description));
+  }, []);
+
   if (!challenge) {
     // TODO implement a 404 page
     // this looks good: https://ant.design/components/result/#components-result-demo-404
@@ -22,9 +36,6 @@ export default function ChallengeDetailView({ serverUrl, address, userProvider }
         <Heading as="h1" mb="2">
           {challenge.label}
         </Heading>
-        <Text color="gray.700" mb="6">
-          {challenge.description}
-        </Text>
         <Button
           as="a"
           colorScheme="gray"
@@ -33,9 +44,10 @@ export default function ChallengeDetailView({ serverUrl, address, userProvider }
           target="_blank"
           rel="noopener noreferrer"
         >
-          Link to challenge <ExternalLinkIcon ml={1} />
+          View it on Github <ExternalLinkIcon ml={1} />
         </Button>
       </Container>
+      <ReactMarkdown>{description}</ReactMarkdown>
       <Container maxW="container.sm" mt={10} centerContent>
         <ChallengeSubmission
           challenge={challenge}
