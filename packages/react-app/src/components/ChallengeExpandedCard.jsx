86ca9a20e31bc36e8b7d0c81c5f82358b1059c15
Carlos Sánchez <oceanrdn@gmail.com>
2021-11-10 10:06:56 +0100

    Show challenges on its own row (instead of grid)

--- packages/react-app/src/components/ChallengeExpandedCard.jsx
+++ packages/react-app/src/components/ChallengeExpandedCard.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { Link as RouteLink } from "react-router-dom";
-import { chakra, Box, Button, Center, Flex, Spacer, Text } from "@chakra-ui/react";
+import { chakra, Button, Center, Flex, Spacer, Text } from "@chakra-ui/react";
 
-const ChallengeExpandedCard = ({ challengeId, challenge }) => (
-  <Box borderWidth="1px" borderRadius="lg" borderColor="gray.200" overflow="hidden">
-    <Center bgColor="gray.200" borderBottom="1px" borderColor="gray.200" h="200px">
-      <Text>{challengeId} image</Text>
+const ChallengeExpandedCard = ({ challengeId, challenge, challengeIndex }) => (
+  <Flex borderWidth="1px" borderRadius="lg" borderColor="gray.200" overflow="hidden" mb={6}>
+    <Center bgColor="gray.200" borderBottom="1px" borderColor="gray.200" w="200px" h="200px" flexShrink={0}>
+      <Text p={3} textAlign="center">
+        {challengeId} image
+      </Text>
     </Center>
-    <Flex pt={9} pb={4} px={4} direction="column" minH="240px">
-      <Text fontWeight="bold">{challenge.label}</Text>
-      <Text color="gray.600">{challenge.description}</Text>
+    <Flex pt={9} pb={4} px={4} direction="column" grow={1}>
+      <Text fontWeight="bold">
+        Challenge {challengeIndex + 1}: {challenge.label}
+      </Text>
+      <Text color="gray.600" mb={4}>
+        {challenge.description}
+      </Text>
       <Spacer />
       <RouteLink to={`/challenge/${challengeId}`}>
         <Button variant="outline" isFullWidth>
-          <span role="img" aria-label="castle icon">⚔️</span>
+          <span role="img" aria-label="castle icon">
+            ⚔️
+          </span>
           <chakra.span ml={1}>Quest</chakra.span>
         </Button>
       </RouteLink>
     </Flex>
-  </Box>
+  </Flex>
 );
 
 export default ChallengeExpandedCard;
--- packages/react-app/src/views/HomeView.jsx
+++ packages/react-app/src/views/HomeView.jsx
@@ -1,33 +1,31 @@
 import React from "react";
-import { Container, SimpleGrid, Heading, Text } from "@chakra-ui/react";
+import { Container, Box, Heading, Text } from "@chakra-ui/react";
 import ChallengeExpandedCard from "../components/ChallengeExpandedCard";
-// ToDo. Ignoring challenge secuence for now.
+// ToDo. Ignoring challenge sequence for now.
 import { challengeInfo } from "../data/challenges";
 
 export default function HomeView() {
   return (
-    <Container maxW="container.xl" centerContent>
-      <Container maxW="container.md">
-        <Text color="gray.700" mb="12" fontSize="xl" textAlign="center">
-          <span role="img" aria-label="castle icon">
-            🏃‍♀️
-          </span>{" "}
-          Speed Run Ethereum aims to provide a structured learning path for onboarding developers to Ethereum with{" "}
-          <span role="img" aria-label="crane icon">
-            🏗
-          </span>
-          scaffold-eth
-        </Text>
-      </Container>
+    <Container maxW="container.md" centerContent>
+      <Text color="gray.700" mb="12" fontSize="xl" textAlign="center">
+        <span role="img" aria-label="castle icon">
+          🏃‍♀️
+        </span>{" "}
+        Speed Run Ethereum aims to provide a structured learning path for onboarding developers to Ethereum with{" "}
+        <span role="img" aria-label="crane icon">
+          🏗
+        </span>
+        scaffold-eth
+      </Text>
       <Heading as="h2">All Challenges</Heading>
       <Text color="gray.700" mb="6">
         Complete all our Ethereum challenges!
       </Text>
-      <SimpleGrid columns={[1, null, 2, null, 3]} spacing={6} pb={20}>
-        {Object.entries(challengeInfo).map(([challengeId, challenge]) => (
-          <ChallengeExpandedCard challengeId={challengeId} challenge={challenge} />
+      <Box>
+        {Object.entries(challengeInfo).map(([challengeId, challenge], index) => (
+          <ChallengeExpandedCard challengeId={challengeId} challenge={challenge} challengeIndex={index} />
         ))}
-      </SimpleGrid>
+      </Box>
     </Container>
   );
 }
