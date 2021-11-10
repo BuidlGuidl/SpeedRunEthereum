06877d7d9a42616854614a0e788851ef0f32a6a0
Carlos Sánchez <oceanrdn@gmail.com>
2021-11-10 10:43:04 +0100

    Challenge preview Image

--- packages/react-app/src/components/ChallengeExpandedCard.jsx
+++ packages/react-app/src/components/ChallengeExpandedCard.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link as RouteLink } from "react-router-dom";
-import { chakra, Button, Center, Flex, Spacer, Text } from "@chakra-ui/react";
+import { chakra, Button, Center, Image, Flex, Spacer, Text } from "@chakra-ui/react";
 
 const ChallengeExpandedCard = ({ challengeId, challenge, challengeIndex }) => (
   <Flex borderWidth="1px" borderRadius="lg" borderColor="gray.200" overflow="hidden" mb={6}>
     <Center bgColor="gray.200" borderBottom="1px" borderColor="gray.200" w="200px" h="200px" flexShrink={0}>
-      <Text p={3} textAlign="center">
-        {challengeId} image
-      </Text>
+      {challenge.previewImage ? (
+        <Image src={challenge.previewImage} objectFit="cover" />
+      ) : (
+        <Text p={3} textAlign="center">
+          {challengeId} image
+        </Text>
+      )}
     </Center>
-    <Flex pt={9} pb={4} px={4} direction="column" grow={1}>
+    <Flex pt={6} pb={4} px={4} direction="column" grow={1}>
       <Text fontWeight="bold">
         Challenge {challengeIndex + 1}: {challenge.label}
       </Text>
