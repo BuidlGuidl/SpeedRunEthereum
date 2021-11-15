16b025d0c3dafa2a60a19b905cc4b8a9d4af8985
Daniel Gonzalez Reina <dangonrei@gmail.com>
2021-11-15 08:47:16 +0100

    Add moon and sun icons

--- packages/react-app/src/components/ColorModeSwitcher.jsx
+++ packages/react-app/src/components/ColorModeSwitcher.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Switch, useColorMode } from "@chakra-ui/react"
+import { Flex, Switch, useColorMode } from "@chakra-ui/react";
+import MoonIcon from "./icons/MoonIcon";
+import SunIcon from "./icons/SunIcon";
 
 const ColorModeSwitcher = () => {
-  const { toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isLightMode = colorMode === "light";
 
   return (
-    <Switch
-      onChange={toggleColorMode}
-      pos="fixed"
-      bottom={0}
-      right={0}
-      p={6}
-    />
+    <Flex pos="fixed" bottom={0} right={0} p={6}>
+      <Switch onChange={toggleColorMode} mr={4} align="center" />
+      {isLightMode ? <SunIcon color="gray.400" h={4} w={4} /> : <MoonIcon h={4} w={4} />}
+    </Flex>
   );
 };
 
