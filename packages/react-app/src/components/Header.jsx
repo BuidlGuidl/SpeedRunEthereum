9dfe17748e5eb3697675793aae0bfd998d39b578
Carlos Sánchez <oceanrdn@gmail.com>
2021-11-22 13:41:21 +0100

    Header RWD tweaks

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -21,17 +21,27 @@ export default function Header({
     injectedProvider && injectedProvider.getSigner && injectedProvider.getSigner()._isSigner;
 
   return (
-    <Box borderBottom="1px" borderColor={borderColor} mb={10} px={8} h={20}>
-      <Flex align="center" h="full" fontWeight="semibold">
-        <HStack as="ul" spacing={9} style={{ listStyle: "none" }}>
-          <chakra.li key="/">
-            <NavLink to="/" exact>
-              <span role="img" aria-label="castle icon">
-                🏃‍♀️
-              </span>{" "}
-              <strong>Speed Run Ethereum</strong>
-            </NavLink>
-          </chakra.li>
+    <Box borderBottom="1px" borderColor={borderColor} mb={10} px={{ base: 4, lg: 8 }} h={{ base: "120px", lg: "80px" }}>
+      <Flex align={{ base: "start", lg: "center" }} h="full" fontWeight="semibold" pos="relative">
+        <Flex shrink={0} mr={9} mt={{ base: 5, lg: 0 }}>
+          <NavLink to="/" exact>
+            <span role="img" aria-label="castle icon">
+              🏃‍♀️
+            </span>{" "}
+            <chakra.strong display={{ base: "none", md: "inline-block" }}>Speed Run Ethereum</chakra.strong>
+            <chakra.strong display={{ base: "inline-block", md: "none" }}>SRE</chakra.strong>
+          </NavLink>
+        </Flex>
+        <HStack
+          as="ul"
+          mr={{ base: 0, lg: 6 }}
+          style={{ listStyle: "none" }}
+          spacing={{ base: 6, lg: 9 }}
+          pos={{ base: "absolute", lg: "static" }}
+          justifyContent={{ base: "center", lg: "left" }}
+          top="80px"
+          left={0}
+        >
           {USER_ROLES.anonymous !== userRole && (
             <chakra.li key="/portfolio" color={secondaryFontColor} _hover={{ color: primaryColorString }}>
               <NavLink
@@ -87,20 +97,22 @@ export default function Header({
           )}
         </HStack>
         <Spacer />
-        <Account
-          address={address}
-          connectText="Connect Wallet"
-          ensProvider={mainnetProvider}
-          isWalletConnected={isSignerProviderConnected}
-          loadWeb3Modal={loadWeb3Modal}
-          logoutOfWeb3Modal={() => {
-            logoutOfWeb3Modal();
-            setUserRole(null);
-          }}
-          setUserRole={setUserRole}
-          userProvider={userProvider}
-          userRole={userRole}
-        />
+        <Box mt={{ base: 3, lg: 0 }}>
+          <Account
+            address={address}
+            connectText="Connect Wallet"
+            ensProvider={mainnetProvider}
+            isWalletConnected={isSignerProviderConnected}
+            loadWeb3Modal={loadWeb3Modal}
+            logoutOfWeb3Modal={() => {
+              logoutOfWeb3Modal();
+              setUserRole(null);
+            }}
+            setUserRole={setUserRole}
+            userProvider={userProvider}
+            userRole={userRole}
+          />
+        </Box>
       </Flex>
     </Box>
   );
