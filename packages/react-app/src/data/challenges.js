10b03afff8edf4a5b60cfad4a6061355f5df49e6
Carlos Sánchez <oceanrdn@gmail.com>
2021-12-02 17:18:55 +0100

    use numbers on badgeId

--- packages/react-app/src/data/challenges.js
+++ packages/react-app/src/data/challenges.js
@@ -5,56 +5,56 @@ export const challengeInfo = {
     disabled: false,
     description: "Build a simple NFT example",
     previewImage: "https://user-images.githubusercontent.com/526558/124386983-48965300-dcb3-11eb-88a7-e88ad6307976.png",
-    badgeId: "0"
+    badgeId: 0,
   },
   "decentralized-staking": {
     branchName: "challenge-1-decentralized-staking",
     label: "Decentralized Staking",
     disabled: false,
     description: "Build a decentralized staking",
-    badgeId: "1"
+    badgeId: 1,
   },
   "token-vendor": {
     branchName: "challenge-2-token-vendor",
     label: "Token Vendor",
     disabled: false,
     description: "Build a token vendor",
-    badgeId: "2"
+    badgeId: 2,
   },
   "learn-multisig": {
     branchName: "challenge-3-multi-sig",
     label: "Learn Multisig",
     disabled: false,
     description: "tbd",
-    badgeId: "3"
+    badgeId: 3,
   },
   "learn-oracles": {
     branchName: "challenge-4-oracle",
     label: "Learn Oracles",
     disabled: false,
     description: "tbd",
-    badgeId: "4"
+    badgeId: 4,
   },
   "minimum-viable-exchange": {
     branchName: "challenge-5-dex",
     label: "Minimum Viable Exchange",
     disabled: false,
     description: "Build a minimum viable exchange",
-    badgeId: "5"
+    badgeId: 5,
   },
   "buyer-mints-nft": {
     branchName: "challenge-6-nft-marketplace",
     label: "Buyer Mints NFT",
     disabled: false,
     description: "Extend the buyer mints branch to have a price",
-    badgeId: "6"
+    badgeId: 6,
   },
   "create-stream": {
     branchName: "challenge-7-create-stream",
     label: "Create a stream",
     disabled: false,
     description: "tbd",
-    badgeId: "7"
+    badgeId: 7,
   },
 };
 
