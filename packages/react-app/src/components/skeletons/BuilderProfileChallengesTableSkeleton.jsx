a0288ece1705f2a360e19ac3bc1e5b325c1b2e34
Daniel Gonzalez Reina <dangonrei@gmail.com>
2021-12-16 12:45:04 +0100

    Improve profile view skeleton

--- packages/react-app/src/components/skeletons/BuilderProfileChallengesTableSkeleton.jsx
+++ packages/react-app/src/components/skeletons/BuilderProfileChallengesTableSkeleton.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Box, Table, Thead, Tbody, Tr, Th, Td, SkeletonText } from "@chakra-ui/react";
+import { Box, Table, Thead, Tbody, Tr, Th, Td, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const BuilderProfileChallengesTableSkeleton = () => (
   <Box overflowX="auto">
     <Table>
       <Thead>
         <Tr>
-          <Th>Name</Th>
+          <Th w="30%">Name</Th>
           <Th>Contract</Th>
           <Th>Live Demo</Th>
           <Th>Updated</Th>
@@ -18,19 +18,21 @@ const BuilderProfileChallengesTableSkeleton = () => (
           return (
             <Tr key={lineNumber}>
               <Td>
-                <SkeletonText noOfLines={1} py={4} />
+                <SkeletonText noOfLines={1} py={2} />
               </Td>
               <Td>
-                <SkeletonText noOfLines={1} py={4} />
+                <SkeletonText noOfLines={1} w="50%" />
               </Td>
               <Td>
-                <SkeletonText noOfLines={1} py={4} />
+                <SkeletonText noOfLines={1} w="50%" />
               </Td>
               <Td>
-                <SkeletonText noOfLines={1} py={4} />
+                <SkeletonText noOfLines={1} />
               </Td>
               <Td>
-                <SkeletonText noOfLines={1} py={4} />
+                <Skeleton h={6} w={20} borderRadius="full">
+                  Submitted
+                </Skeleton>
               </Td>
             </Tr>
           );
