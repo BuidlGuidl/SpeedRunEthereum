211ca0d6bc0d40c4a9951f96bf45f60f6daf3d0c
Daniel Gonzalez Reina <dangonrei@gmail.com>
2021-12-17 13:24:00 +0100

    Fix bug showing always the same time

--- packages/react-app/src/components/DateWithTooltip.jsx
+++ packages/react-app/src/components/DateWithTooltip.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import moment from "moment";
 import { Box, Tooltip } from "@chakra-ui/react";
 
-const DateWithTooltip = timestamp => {
+const DateWithTooltip = ({ timestamp }) => {
   const timestampMoment = moment(timestamp);
   return (
     <Tooltip label={timestampMoment.format("YYYY-MM-DD, HH:mm")}>
