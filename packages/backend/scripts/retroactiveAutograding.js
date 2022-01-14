389a44de75e554677b893756cb13e563b9d6fb80
Carlos Sánchez <oceanrdn@gmail.com>
2022-01-14 18:51:58 +0100

    Update the retroactive autograder script

--- packages/backend/scripts/retroactiveAutograding.js
+++ packages/backend/scripts/retroactiveAutograding.js
@@ -3,6 +3,10 @@ const axios = require("axios");
 const db = require("../services/db");
 const { getChallengeIndexFromChallengeId, isAutogradingEnabledForChallenge } = require("../utils/challenges");
 
+let acceptedChallenges = 0;
+let rejectedChallenges = 0;
+let autograderErrors = 0;
+
 (async () => {
   const allChallenges = await db.getAllChallenges();
   const pendingChallenges = allChallenges.filter(({ status: challengeStatus }) => challengeStatus === "SUBMITTED");
@@ -32,23 +36,47 @@ const { getChallengeIndexFromChallengeId, isAutogradingEnabledForChallenge } = r
           network,
           address: contractAddress,
         })
-        .then(gradingResponse => {
+        .then(async gradingResponse => {
           // We don't wait for the auto grading to finish to return a response.
           const gradingResponseData = gradingResponse.data;
           console.log("auto-grading response data", gradingResponseData);
 
           if (gradingResponseData) {
-            existingChallenges[challenge.id].status = gradingResponseData.success ? "ACCEPTED" : "REJECTED";
             existingChallenges[challenge.id].reviewComment = gradingResponseData.feedback;
+            existingChallenges[challenge.id].autograding = true;
+
+            // For now just auto-grade accepted submissions, so rejections will always be manually reviewed by graders.
+            // We store the autograder feedback.
+            if (gradingResponseData.success) {
+              console.log("✔️ Accepted!");
+              existingChallenges[challenge.id].status = "ACCEPTED";
+              acceptedChallenges += 1;
+            } else {
+              console.log("❌ Rejected!");
+              rejectedChallenges += 1;
+            }
+          }
+        })
+        .catch(gradingErrorResponse => {
+          const gradingErrorResponseData = gradingErrorResponse?.response?.data;
+
+          // We don't change the status of the submission, just leave the error for the manual graders to see.
+          if (gradingErrorResponseData) {
+            existingChallenges[challenge.id].reviewComment = `Autograder: ${gradingErrorResponseData.error}`;
             existingChallenges[challenge.id].autograding = true;
           }
 
-          // ToDo. Uncomment when ready.
-          // db.updateUser(challenge.userAddress, { challenges: existingChallenges });
+          autograderErrors += 1;
+          console.error("❌❌❌ auto-grading failed:", gradingErrorResponseData?.error);
         })
-        .catch(error => {
-          console.error("auto-grading failed", error);
+        .then(async () => {
+          await db.updateUser(challenge.userAddress, { challenges: existingChallenges }); // INFO: async, no await here.
         });
     }
   }
+
+  console.log("\n\n#### Retroactive Autograding results\n");
+  console.log("✔️  Accepted challenges:", acceptedChallenges, "\n");
+  console.log("❌ Rejected challenges:", rejectedChallenges, " (not saved in the DB)\n");
+  console.log("❗️ Autograder errors:", autograderErrors, "\n");
 })();
