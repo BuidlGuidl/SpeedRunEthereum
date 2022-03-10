1f5a1b4a97447ee11424f510152a067a00eaa92c
Carlos Sánchez <oceanrdn@gmail.com>
2022-03-10 10:30:14 +0100

    Linting

--- packages/react-app/src/views/HomeView.jsx
+++ packages/react-app/src/views/HomeView.jsx
@@ -20,42 +20,101 @@ export default function HomeView({ connectedBuilder }) {
 
   return (
     <Container maxW="container.lg" centerContent>
-      <div style={{width:740,margin:"auto",border:"1px solid #DDDDDD", padding:32, marginBottom:64}}>
-      <Text color={primaryFontColor} mb="6" fontSize="xl" textAlign="center">
-        <span role="img" aria-label="teacher icon">
-          👩‍🏫
-        </span>{" "}
-        Learn how to build on Ethereum; the superpowers and the gotchas.
-      </Text>
+      <div style={{ width: 740, margin: "auto", border: "1px solid #DDDDDD", padding: 32, marginBottom: 64 }}>
+        <Text color={primaryFontColor} mb="6" fontSize="xl" textAlign="center">
+          <span role="img" aria-label="teacher icon">
+            👩‍🏫
+          </span>{" "}
+          Learn how to build on Ethereum; the superpowers and the gotchas.
+        </Text>
 
-      <Text color={primaryFontColor} mb="6" fontSize="xl" textAlign="center">
-        <span role="img" aria-label="teacher icon">
-          🎥
-        </span>{" "}
-        Watch this quick video as an <a href="https://www.youtube.com/watch?v=MlJPjJQZtC8" target="_blank" style={{textDecoration:"underline"}}>Intro to Ethereum Development</a>.
-      </Text>
+        <Text color={primaryFontColor} mb="6" fontSize="xl" textAlign="center">
+          <span role="img" aria-label="teacher icon">
+            🎥
+          </span>{" "}
+          Watch this quick video as an{" "}
+          <a
+            href="https://www.youtube.com/watch?v=MlJPjJQZtC8"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "underline" }}
+          >
+            Intro to Ethereum Development
+          </a>
+          .
+        </Text>
 
-      <Text color={primaryFontColor} mb="2" fontSize="xl" textAlign="center">
-        Then use {" "}
-        <a style={{textDecoration:"underline"}} href="https://github.com/scaffold-eth/scaffold-eth#-scaffold-eth" target="_blank"><span role="img" aria-label="teacher icon">
-          🏗
-        </span>{" "}
-        Scaffold-ETH</a> to copy/paste each Solidity concept and tinker:
-      </Text>
+        <Text color={primaryFontColor} mb="2" fontSize="xl" textAlign="center">
+          Then use{" "}
+          <a
+            style={{ textDecoration: "underline" }}
+            href="https://github.com/scaffold-eth/scaffold-eth#-scaffold-eth"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <span role="img" aria-label="teacher icon">
+              🏗
+            </span>{" "}
+            Scaffold-ETH
+          </a>{" "}
+          to copy/paste each Solidity concept and tinker:
+        </Text>
 
-      <Text color={primaryFontColor} mb="8" fontSize="xl" textAlign="center">
-        <div>
-          <a href="https://docs.soliditylang.org/en/v0.6.6/units-and-global-variables.html" target="_blank">global units</a>, <a target="_blank" href="https://solidity-by-example.org/primitives/">primitives</a>, <a target="_blank" href="https://solidity-by-example.org/mapping/">mappings</a>,
-         {" "}<a href="https://solidity-by-example.org/structs/" target="_blank">structs</a>, <a href="https://solidity-by-example.org/function-modifier/" target="_blank">modifiers</a>, <a href="https://solidity-by-example.org/events/" target="_blank">events</a>,</div> <a href="https://solidity-by-example.org/inheritance/" target="_blank">inheritance</a>, <a href="https://solidity-by-example.org/sending-ether/" target="_blank">sending eth</a>, and <a href="https://solidity-by-example.org/payable/" target="_blank">payable</a>/<a href="https://solidity-by-example.org/fallback/" target="_blank">fallback</a> functions.
-      </Text>
-      <Text color={primaryFontColor} mb="0" fontSize="xl" textAlign="center">
-        <span role="img" aria-label="teacher icon">
-          🧑‍🚀
-        </span>{" "}
-        When you are ready to test your knowledge, speed run Ethereum:
-      </Text>
+        <Text color={primaryFontColor} mb="8" fontSize="xl" textAlign="center">
+          <div>
+            <a
+              href="https://docs.soliditylang.org/en/v0.6.6/units-and-global-variables.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              global units
+            </a>
+            ,{" "}
+            <a target="_blank" href="https://solidity-by-example.org/primitives/" rel="noopener noreferrer">
+              primitives
+            </a>
+            ,{" "}
+            <a target="_blank" href="https://solidity-by-example.org/mapping/" rel="noopener noreferrer">
+              mappings
+            </a>
+            ,{" "}
+            <a href="https://solidity-by-example.org/structs/" target="_blank" rel="noopener noreferrer">
+              structs
+            </a>
+            ,{" "}
+            <a href="https://solidity-by-example.org/function-modifier/" target="_blank" rel="noopener noreferrer">
+              modifiers
+            </a>
+            ,{" "}
+            <a href="https://solidity-by-example.org/events/" target="_blank" rel="noopener noreferrer">
+              events
+            </a>
+            ,
+          </div>{" "}
+          <a href="https://solidity-by-example.org/inheritance/" target="_blank" rel="noopener noreferrer">
+            inheritance
+          </a>
+          ,{" "}
+          <a href="https://solidity-by-example.org/sending-ether/" target="_blank" rel="noopener noreferrer">
+            sending eth
+          </a>
+          , and{" "}
+          <a href="https://solidity-by-example.org/payable/" target="_blank" rel="noopener noreferrer">
+            payable
+          </a>
+          /
+          <a href="https://solidity-by-example.org/fallback/" target="_blank" rel="noopener noreferrer">
+            fallback
+          </a>{" "}
+          functions.
+        </Text>
+        <Text color={primaryFontColor} mb="0" fontSize="xl" textAlign="center">
+          <span role="img" aria-label="teacher icon">
+            🧑‍🚀
+          </span>{" "}
+          When you are ready to test your knowledge, speed run Ethereum:
+        </Text>
       </div>
-
 
       <Box>
         {Object.entries(challengeInfo).map(([challengeId, challenge], index) => (
