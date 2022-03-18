9ff3b826b4801a2e7228580542ae72822b56fa31
Carlos Sánchez <oceanrdn@gmail.com>
2022-03-18 11:19:06 +0100

    Link to BG profile

--- packages/react-app/src/views/BuilderProfileView.jsx
+++ packages/react-app/src/views/BuilderProfileView.jsx
@@ -23,12 +23,14 @@ import {
   GridItem,
   Tag,
   SkeletonText,
+  Image,
 } from "@chakra-ui/react";
 import { InfoOutlineIcon } from "@chakra-ui/icons";
 import BuilderProfileCard from "../components/BuilderProfileCard";
 import BuilderProfileChallengesTableSkeleton from "../components/skeletons/BuilderProfileChallengesTableSkeleton";
 import { challengeInfo } from "../data/challenges";
 import { userFunctionDescription } from "../helpers/constants";
+import { BG_BACKEND_URL as bgBackendUrl } from "../constants";
 import ChallengeStatusTag from "../components/ChallengeStatusTag";
 import { getAcceptedChallenges } from "../helpers/builders";
 import useCustomColorModes from "../hooks/useCustomColorModes";
@@ -36,12 +38,15 @@ import { getChallengeEventsForUser } from "../data/api";
 import { byTimestamp } from "../helpers/sorting";
 import DateWithTooltip from "../components/DateWithTooltip";
 
+const BG_FRONTEND_URL = "https://bazaar.buidlguidl.com";
+
 export default function BuilderProfileView({ serverUrl, mainnetProvider, address, userProvider, userRole }) {
   const { builderAddress } = useParams();
   const { primaryFontColor, secondaryFontColor, borderColor, iconBgColor } = useCustomColorModes();
   const [builder, setBuilder] = useState();
   const [challengeEvents, setChallengeEvents] = useState([]);
   const [isLoadingBuilder, setIsLoadingBuilder] = useState(false);
+  const [isBuilderOnBg, setIsBuilderOnBg] = useState(false);
   const [isLoadingTimestamps, setIsLoadingTimestamps] = useState(false);
   const toast = useToast({ position: "top", isClosable: true });
   const toastVariant = useColorModeValue("subtle", "solid");
@@ -53,6 +58,16 @@ export default function BuilderProfileView({ serverUrl, mainnetProvider, address
     setIsLoadingBuilder(true);
     const fetchedBuilder = await axios.get(serverUrl + `/builders/${builderAddress}`);
     setBuilder(fetchedBuilder.data);
+
+    try {
+      await axios.get(bgBackendUrl + `/builders/${builderAddress}`);
+    } catch (e) {
+      // Builder Not found in BG
+      setIsLoadingBuilder(false);
+      return;
+    }
+
+    setIsBuilderOnBg(true);
     setIsLoadingBuilder(false);
   };
 
@@ -97,156 +112,172 @@ export default function BuilderProfileView({ serverUrl, mainnetProvider, address
             userRole={userRole}
           />
         </GridItem>
-        <GridItem colSpan={{ base: 1, xl: 3 }}>
-          <HStack spacing={4} mb={8}>
-            <Flex borderRadius="lg" borderColor={borderColor} borderWidth={1} p={4} w="full" justify="space-between">
-              <Flex bg={iconBgColor} borderRadius="lg" w={12} h={12} justify="center" align="center">
-                <InfoOutlineIcon w={5} h={5} />
+        {isBuilderOnBg ? (
+          <GridItem colSpan={{ base: 1, xl: 3 }}>
+            <Box borderColor={borderColor} borderWidth={1} p={5}>
+              <Flex direction="column" align="center" justify="center">
+                <Image src="/assets/bg.png" mb={3} />
+                <Text mb={3} fontSize="lg" fontWeight="bold">
+                  This builder has upgraded to BuidlGuidl.
+                </Text>
+                <Button as={Link} href={`${BG_FRONTEND_URL}/builders/${builderAddress}`} isExternal colorScheme="blue">
+                  View their profile on Buidlguidl
+                </Button>
               </Flex>
-              <div>
-                <Text fontSize="xl" fontWeight="medium" textAlign="right">
-                  {acceptedChallenges.length}
-                </Text>
-                <Text fontSize="sm" color={secondaryFontColor} textAlign="right">
-                  challenges completed
-                </Text>
-              </div>
-            </Flex>
-            <Flex borderRadius="lg" borderColor={borderColor} borderWidth={1} p={4} w="full" justify="space-between">
-              <Flex bg={iconBgColor} borderRadius="lg" w={12} h={12} justify="center" align="center">
-                <InfoOutlineIcon w={5} h={5} />
+            </Box>
+          </GridItem>
+        ) : (
+          <GridItem colSpan={{ base: 1, xl: 3 }}>
+            <HStack spacing={4} mb={8}>
+              <Flex borderRadius="lg" borderColor={borderColor} borderWidth={1} p={4} w="full" justify="space-between">
+                <Flex bg={iconBgColor} borderRadius="lg" w={12} h={12} justify="center" align="center">
+                  <InfoOutlineIcon w={5} h={5} />
+                </Flex>
+                <div>
+                  <Text fontSize="xl" fontWeight="medium" textAlign="right">
+                    {acceptedChallenges.length}
+                  </Text>
+                  <Text fontSize="sm" color={secondaryFontColor} textAlign="right">
+                    challenges completed
+                  </Text>
+                </div>
               </Flex>
-              <div>
-                <Text fontSize="xl" fontWeight="medium" textAlign="right">
-                  {builder?.function ? (
-                    <Tag colorScheme={userFunctionDescription[builder?.function].colorScheme} variant="solid">
-                      {userFunctionDescription[builder?.function].label}
-                    </Tag>
-                  ) : (
-                    "-"
-                  )}
-                </Text>
-                <Text fontSize="sm" color={secondaryFontColor} textAlign="right">
-                  Role
-                </Text>
-              </div>
+              <Flex borderRadius="lg" borderColor={borderColor} borderWidth={1} p={4} w="full" justify="space-between">
+                <Flex bg={iconBgColor} borderRadius="lg" w={12} h={12} justify="center" align="center">
+                  <InfoOutlineIcon w={5} h={5} />
+                </Flex>
+                <div>
+                  <Text fontSize="xl" fontWeight="medium" textAlign="right">
+                    {builder?.function ? (
+                      <Tag colorScheme={userFunctionDescription[builder?.function].colorScheme} variant="solid">
+                        {userFunctionDescription[builder?.function].label}
+                      </Tag>
+                    ) : (
+                      "-"
+                    )}
+                  </Text>
+                  <Text fontSize="sm" color={secondaryFontColor} textAlign="right">
+                    Role
+                  </Text>
+                </div>
+              </Flex>
+            </HStack>
+            <Flex mb={4}>
+              <Text fontSize="2xl" fontWeight="bold">
+                Challenges
+              </Text>
+              <Spacer />
             </Flex>
-          </HStack>
-          <Flex mb={4}>
-            <Text fontSize="2xl" fontWeight="bold">
-              Challenges
-            </Text>
-            <Spacer />
-          </Flex>
-          {isLoadingBuilder && <BuilderProfileChallengesTableSkeleton />}
-          {!isLoadingBuilder &&
-            (challenges ? (
-              <Box overflowX="auto">
-                <Table>
-                  {isMyProfile && (
-                    <TableCaption>
+            {isLoadingBuilder && <BuilderProfileChallengesTableSkeleton />}
+            {!isLoadingBuilder &&
+              (challenges ? (
+                <Box overflowX="auto">
+                  <Table>
+                    {isMyProfile && (
+                      <TableCaption>
+                        <Button as={RouteLink} colorScheme="blue" to="/">
+                          Start a challenge
+                        </Button>
+                      </TableCaption>
+                    )}
+                    <Thead>
+                      <Tr>
+                        <Th>Name</Th>
+                        <Th>Contract</Th>
+                        <Th>Live Demo</Th>
+                        <Th>Updated</Th>
+                        <Th>Status</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      {challenges.map(([challengeId, lastSubmission]) => {
+                        if (!challengeInfo[challengeId]) {
+                          return null;
+                        }
+                        const lastEventForChallenge = challengeEvents.filter(
+                          event => event.payload.challengeId === challengeId,
+                        )[0];
+                        return (
+                          <Tr key={challengeId}>
+                            <Td>
+                              <Link as={RouteLink} to={`/challenge/${challengeId}`} fontWeight="700" color="teal.500">
+                                {challengeInfo[challengeId].label}
+                              </Link>
+                            </Td>
+                            <Td>
+                              <Link
+                                // Legacy branchUrl
+                                href={lastSubmission.contractUrl || lastSubmission.branchUrl}
+                                color="teal.500"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                Code
+                              </Link>
+                            </Td>
+                            <Td>
+                              <Link
+                                href={lastSubmission.deployedUrl}
+                                color="teal.500"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                Demo
+                              </Link>
+                            </Td>
+                            <Td>
+                              {isLoadingTimestamps ? (
+                                <SkeletonText noOfLines={1} />
+                              ) : (
+                                <DateWithTooltip timestamp={lastEventForChallenge?.timestamp} />
+                              )}
+                            </Td>
+                            <Td>
+                              <ChallengeStatusTag
+                                status={lastSubmission.status}
+                                comment={lastSubmission.reviewComment}
+                                autograding={lastSubmission.autograding}
+                              />
+                            </Td>
+                          </Tr>
+                        );
+                      })}
+                    </Tbody>
+                  </Table>
+                </Box>
+              ) : (
+                <Flex
+                  justify="center"
+                  align="center"
+                  borderRadius="lg"
+                  borderColor={borderColor}
+                  borderWidth={1}
+                  py={36}
+                  w="full"
+                >
+                  {isMyProfile ? (
+                    <Box maxW="xs" textAlign="center">
+                      <Text fontWeight="medium" color={primaryFontColor} mb={2}>
+                        Start a new challenge
+                      </Text>
+                      <Text color={secondaryFontColor} mb={4}>
+                        Show off your skills. Learn everything you need to build on Ethereum!
+                      </Text>
                       <Button as={RouteLink} colorScheme="blue" to="/">
                         Start a challenge
                       </Button>
-                    </TableCaption>
+                    </Box>
+                  ) : (
+                    <Box maxW="xs" textAlign="center">
+                      <Text color={secondaryFontColor} mb={4}>
+                        This builder hasn't completed any challenges.
+                      </Text>
+                    </Box>
                   )}
-                  <Thead>
-                    <Tr>
-                      <Th>Name</Th>
-                      <Th>Contract</Th>
-                      <Th>Live Demo</Th>
-                      <Th>Updated</Th>
-                      <Th>Status</Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    {challenges.map(([challengeId, lastSubmission]) => {
-                      if (!challengeInfo[challengeId]) {
-                        return null;
-                      }
-                      const lastEventForChallenge = challengeEvents.filter(
-                        event => event.payload.challengeId === challengeId,
-                      )[0];
-                      return (
-                        <Tr key={challengeId}>
-                          <Td>
-                            <Link as={RouteLink} to={`/challenge/${challengeId}`} fontWeight="700" color="teal.500">
-                              {challengeInfo[challengeId].label}
-                            </Link>
-                          </Td>
-                          <Td>
-                            <Link
-                              // Legacy branchUrl
-                              href={lastSubmission.contractUrl || lastSubmission.branchUrl}
-                              color="teal.500"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              Code
-                            </Link>
-                          </Td>
-                          <Td>
-                            <Link
-                              href={lastSubmission.deployedUrl}
-                              color="teal.500"
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              Demo
-                            </Link>
-                          </Td>
-                          <Td>
-                            {isLoadingTimestamps ? (
-                              <SkeletonText noOfLines={1} />
-                            ) : (
-                              <DateWithTooltip timestamp={lastEventForChallenge?.timestamp} />
-                            )}
-                          </Td>
-                          <Td>
-                            <ChallengeStatusTag
-                              status={lastSubmission.status}
-                              comment={lastSubmission.reviewComment}
-                              autograding={lastSubmission.autograding}
-                            />
-                          </Td>
-                        </Tr>
-                      );
-                    })}
-                  </Tbody>
-                </Table>
-              </Box>
-            ) : (
-              <Flex
-                justify="center"
-                align="center"
-                borderRadius="lg"
-                borderColor={borderColor}
-                borderWidth={1}
-                py={36}
-                w="full"
-              >
-                {isMyProfile ? (
-                  <Box maxW="xs" textAlign="center">
-                    <Text fontWeight="medium" color={primaryFontColor} mb={2}>
-                      Start a new challenge
-                    </Text>
-                    <Text color={secondaryFontColor} mb={4}>
-                      Show off your skills. Learn everything you need to build on Ethereum!
-                    </Text>
-                    <Button as={RouteLink} colorScheme="blue" to="/">
-                      Start a challenge
-                    </Button>
-                  </Box>
-                ) : (
-                  <Box maxW="xs" textAlign="center">
-                    <Text color={secondaryFontColor} mb={4}>
-                      This builder hasn't completed any challenges.
-                    </Text>
-                  </Box>
-                )}
-              </Flex>
-            ))}
-        </GridItem>
+                </Flex>
+              ))}
+          </GridItem>
+        )}
       </SimpleGrid>
     </Container>
   );
