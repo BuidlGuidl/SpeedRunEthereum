a4d11c9baabe36d80e25cb9c0164b6e018e04793
Shiv Bhonde <shivbhonde04@gmail.com>
2022-11-18 20:51:40 +0530

    configure theme

--- packages/react-app/src/hooks/useCustomColorModes.js
+++ packages/react-app/src/hooks/useCustomColorModes.js
@@ -1,16 +1,19 @@
 import { useColorModeValue } from "@chakra-ui/react";
 
 const useCustomColorModes = () => {
-  const primaryFontColor = useColorModeValue("gray.700", "gray.200");
+  const primaryFontColor = useColorModeValue("sre.text", "sreDark.text");
   const codeFontColor = primaryFontColor;
   const secondaryFontColor = useColorModeValue("gray.600", "gray.400");
-  const dividerColor = useColorModeValue("gray.200", "gray.700");
+  const dividerColor = useColorModeValue("sre.default", "sreDark.default");
   const borderColor = dividerColor;
   const codeBgColor = useColorModeValue("gray.100", "gray.900");
   const iconBgColor = codeBgColor;
   const linkColor = useColorModeValue("#088484", "#C8F5FF");
 
+  const bgColor = useColorModeValue("sre.background", "sreDark.background");
+
   return {
+    bgColor,
     primaryFontColor,
     secondaryFontColor,
     dividerColor,
--- packages/react-app/src/theme.js
+++ packages/react-app/src/theme.js
@@ -4,6 +4,18 @@ const theme = extendTheme({
   initialColorMode: "light",
   useSystemColorMode: false,
   colors: {
+    sre: {
+      default: "#088484",
+      text: "#026262",
+      background: "#C8F5FF",
+      cardBackground: "#E9FBFF",
+    },
+    sreDark: {
+      default: "#C8F5FF",
+      text: "#C8F5FF",
+      background: "#088484",
+      cardBackground: "#026262",
+    },
     green: {
       500: "#088484",
     },
