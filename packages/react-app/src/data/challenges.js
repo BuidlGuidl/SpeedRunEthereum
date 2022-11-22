562f6b177aee3d6d44d8e9d8b71a77db1f155b97
Shiv Bhonde <shivbhonde04@gmail.com>
2022-11-22 20:34:30 +0530

    add challenge number back to challenge data

--- packages/react-app/src/data/challenges.js
+++ packages/react-app/src/data/challenges.js
@@ -2,7 +2,7 @@ export const challengeInfo = {
   "simple-nft-example": {
     id: 0,
     branchName: "challenge-0-simple-nft",
-    label: "🎟 Simple NFT Example",
+    label: "🚩 Challenge 0: 🎟 Simple NFT Example",
     disabled: false,
     description:
       "🎫 Create a simple NFT to learn basics of 🏗 scaffold-eth. You'll use 👷‍♀️ HardHat to compile and deploy smart contracts. Then, you'll use a template React app full of important Ethereum components and hooks. Finally, you'll deploy an NFT to a public network to share with friends! 🚀",
@@ -12,7 +12,7 @@ export const challengeInfo = {
   "decentralized-staking": {
     id: 1,
     branchName: "challenge-1-decentralized-staking",
-    label: "🥩 Decentralized Staking App ",
+    label: "🚩 Challenge 1: 🥩 Decentralized Staking App ",
     disabled: false,
     description:
       "🦸 A superpower of Ethereum is allowing you, the builder, to create a simple set of rules that an adversarial group of players can use to work together. In this challenge, you create a decentralized application where users can coordinate a group funding effort. The users only have to trust the code.",
@@ -22,7 +22,7 @@ export const challengeInfo = {
   "token-vendor": {
     id: 2,
     branchName: "challenge-2-token-vendor",
-    label: "🏵 Token Vendor",
+    label: "🚩 Challenge 2: 🏵 Token Vendor",
     disabled: false,
     description:
       '🤖 Smart contracts are kind of like "always on" vending machines that anyone can access. Let\'s make a decentralized, digital currency (an ERC20 token). Then, let\'s build an unstoppable vending machine that will buy and sell the currency. We\'ll learn about the "approve" pattern for ERC20s and how contract to contract interactions work.',
@@ -32,7 +32,7 @@ export const challengeInfo = {
   "dice-game": {
     id: 3,
     branchName: "challenge-3-dice-game",
-    label: "🎲 Dice Game",
+    label: "🚩 Challenge 3: 🎲 Dice Game",
     disabled: false,
     description:
       "🎰 Randomness is tricky on a public deterministic blockchain. The block hash is the result proof-of-work (for now) and some builders use this as a weak form of randomness.  In this challenge you will take advantage of a Dice Game contract by predicting the randomness in order to only roll winning dice!",
